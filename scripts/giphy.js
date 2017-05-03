732dd21926fd951dfe1bd96bd17efcdbd4333558
Tom Fitz <tfitz237@gmail.com>
2017-05-03 16:13:59 -0400

    gif translate 4

--- scripts/giphy.js
+++ scripts/giphy.js
@@ -8,10 +8,10 @@ exports.giphy = function(bot) {
         private: false,
         rtn: (from, message) => {
 
-            let res = request('GET','http://api.giphy.com/v1/gifs/translate?q='+encodeURIComponent(message.replace(' ', '+'))+'&api_key=dc6zaTOxFJmzC');
+            let res = request('GET','http://api.giphy.com/v1/gifs/translate?s='+encodeURIComponent(message.replace(' ', '+'))+'&api_key=dc6zaTOxFJmzC');
             try {
                 let data = JSON.parse(res.getBody());
-                if(data.data)
+		if(data.data)
                     return data.data.url;
                 return 'No media found.';
             } catch (e) {
