ab3121d7f189bdc4b2eab4a61edafa6175068806
Tom Fitzpatrick <tfitz237@gmail.com>
2017-05-03 15:46:42 -0400

    giphy

---
+++ scripts/giphy.js
@@ -0,0 +1,23 @@
+let request = require('sync-request');
+
+exports.giphy = function(bot) {
+    return {
+        name: 'gif search',
+        commands: ['gif', 'giphy'],
+        isCommand: true,
+        private: false,
+        rtn: (from, message) => {
+
+            let res = request('GET','http://api.giphy.com/v1/gifs/search?q='+encodeURIComponent(message)+'&api_key=dc6zaTOxFJmzC');
+            try {
+                let data = JSON.parse(res.getBody());
+                if(data.data)
+                    return data.data[0].url;
+                return 'No media found.';
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }
+
+};
